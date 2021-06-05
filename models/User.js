28f31e1956c70ab09d7316c46555ba2dc5c3901d
JovicAbengona <37503727+JovicAbengona@users.noreply.github.com>
2021-06-05 16:44:45 +0800

    Updated model

--- models/User.js
+++ models/User.js
@@ -1,16 +1,14 @@
 const db = require("../config");
 
-module.exports = class User{
-    static login(email, password){
-        return db.execute(`SELECT * FROM users WHERE email = ? AND password = MD5(?)`, [email, password]);
-    }
-
-    static register(email, first_name, last_name, password){
-        return db.execute(`INSERT INTO users (email, first_name, last_name, password, created_at, updated_at) 
+module.exports = {
+    login: (email, password) => {
+        return db.execute(`SELECT * FROM express_users.users WHERE email = ? AND password = MD5(?)`, [email, password]);
+    },
+    register: (email, first_name, last_name, password) => {
+        return db.execute(`INSERT INTO express_users.users (email, first_name, last_name, password, created_at, updated_at) 
                             VALUES (?, ?, ?, MD5(?), NOW(), NOW())`, [email, first_name, last_name, password]);
-    }
-
-    static checkEmail(email){
-        return db.execute(`SELECT email FROM users WHERE email = ?`, [email]);
+    },
+    checkEmail: (email) => {
+        return db.execute(`SELECT email FROM express_users.users WHERE email = ?`, [email]);
     }
 }
